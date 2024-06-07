f6404979c90d9d2f4451d6a4fa3be702025eadfc
aoirusann <aoirusann@gmail.com>
2024-06-07 11:35:06 +0800

    Init

---
+++ DashscopeApiAsr.py
@@ -0,0 +1,74 @@
+# For prerequisites running the following sample, visit https://help.aliyun.com/document_detail/611472.html
+
+import dashscope
+from dashscope.audio.asr import (Recognition, RecognitionCallback,
+                                 RecognitionResult)
+
+
+class DefaultCallback(RecognitionCallback):
+    def on_open(self) -> None:
+        print('RecognitionCallback open.')
+
+    def on_close(self) -> None:
+        print('RecognitionCallback close.')
+
+    def on_event(self, result: RecognitionResult) -> None:
+        print('RecognitionCallback sentence: ', result.get_sentence())
+
+
+class DashscopeApiAsr:
+    def __init__(self):
+        self.recognition: Recognition = None
+
+    def start(self, api_key: str, callback: RecognitionCallback = DefaultCallback()):
+        dashscope.api_key = api_key
+        self.recognition = Recognition(
+            model='paraformer-realtime-v1',
+            format='pcm',
+            sample_rate=16000,
+            callback=callback
+        )
+        self.recognition.start()
+
+    def stop(self):
+        self.recognition.stop()
+
+    def send_audio_frame(self, audio_data):
+        self.recognition.send_audio_frame(audio_data)
+
+
+
+if __name__ == "__main__":
+    import pyaudio
+    import os
+    asr = DashscopeApiAsr()
+    asr.start(api_key=os.environ.get("API_KEY"))
+
+    mic = pyaudio.PyAudio()
+    device_id2name = {}
+    info = mic.get_host_api_info_by_index(0)
+    numdevices = info.get('deviceCount')
+    for i in range(0, numdevices):
+        if (mic.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+            name = mic.get_device_info_by_host_api_device_index(0, i).get('name')
+            device_id2name[i] = name
+    for i, name in device_id2name.items():
+        print("Input Device id ", i, " - ", name)
+    micro_device_id = int(input("Select Microphone: "))
+
+    stream = mic.open(format=pyaudio.paInt16,
+        channels=1,
+        input_device_index=micro_device_id,
+        rate=16000,
+        input=True
+        )
+
+    try:
+        while True:
+            data = stream.read(3200, exception_on_overflow = False)
+            asr.send_audio_frame(data)
+    finally:
+        asr.stop()
+        stream.stop_stream()
+        stream.close()
+        mic.terminate()
---
+++ main.py
@@ -0,0 +1,223 @@
+from DashscopeApiAsr import DashscopeApiAsr, RecognitionCallback, RecognitionResult
+from nicegui import ui
+import re
+import os
+import pyaudio
+import asyncio
+import pythonosc
+import pythonosc.udp_client
+import multiprocessing
+import logging
+import logging.handlers
+logger = logging.getLogger("VRChatParaformerAsr")
+
+class SettingAndContext:
+    def __init__(self) -> None:
+        # Setting ====
+        # vrchat: should recreate `VRChatOscCallback` and restart `DashscopeApiAsr` after change
+        self.vrchat_ip = r"127.0.0.1"
+        self.vrchat_port = 9000
+        # osc
+        self.osc_bypass_keyboard = True
+        self.osc_enableSFX = True
+        # microphone: should recreate `MicCollector` after change
+        self.micro_device_id = 3
+        # dashscope api: should restart`DashscopeApiAsr` after change
+        self.api_key = ""
+        self.disfluency_removal_enabled = False
+        # Context ====
+        self.stt_worker = None
+
+class VRChatOscCallback(RecognitionCallback):
+    def __init__(self, ctx: SettingAndContext):
+        self.ctx = ctx
+        self.osc_client = pythonosc.udp_client.SimpleUDPClient(self.ctx.vrchat_ip, self.ctx.vrchat_port)
+        self.last_text = ""
+
+    def on_open(self) -> None:
+        logger.info('RecognitionCallback open.')
+
+    def on_close(self) -> None:
+        logger.info('RecognitionCallback close.')
+
+    def on_error(self, result: RecognitionResult) -> None:
+        logger.error(result)
+
+    def on_complete(self) -> None:
+        pass
+
+    def on_event(self, result: RecognitionResult) -> None:
+        # Get full sentence
+        sen = result.get_sentence()
+        logger.debug(f'RecognitionCallback sentence: {sen}', )
+        # If the sentence is completed, update last_text
+        if result.is_sentence_end(sen):
+            # Extract the text
+            cur_text = sen["text"]
+            logger.info(f"[Transcribed] {cur_text}")
+            # Merge with the last complete text
+            text = self.last_text + "\n" + cur_text
+            self.osc_client.send_message("/chatbox/input", [text, self.ctx.osc_bypass_keyboard, self.ctx.osc_enableSFX])
+            # Update last_text
+            self.last_text = cur_text
+
+class MicCollector:
+    def __init__(self, ctx: SettingAndContext):
+        self.ctx = ctx
+        self.mic: pyaudio.PyAudio = None
+        self.stream: pyaudio.Stream = None
+
+    def __del__(self):
+        self.stop()
+
+    def start(self, callback):
+        self.mic = pyaudio.PyAudio()
+        self.stream = self.mic.open(format=pyaudio.paInt16,
+            channels=1,
+            input_device_index=self.ctx.micro_device_id,
+            rate=16000,
+            input=True,
+            stream_callback=callback,
+            frames_per_buffer=3200
+            )
+
+    def stop(self):
+        if self.stream:
+            self.stream.stop_stream()
+            self.stream.close()
+            self.stream = None
+        if self.mic:
+            self.mic.terminate()
+            self.mic = None
+
+# Audio and Speech Recognition Workhorse
+# Keep running until `Stop`
+async def ARSWorker(ctx: SettingAndContext):
+    asr_callback = VRChatOscCallback(ctx)
+    asr = DashscopeApiAsr()
+    asr.start(api_key=ctx.api_key, callback=asr_callback)
+
+    def mic_callback(in_data, frame_count, time_info, status_flags):
+        asr.send_audio_frame(in_data)
+        return None, pyaudio.paContinue
+    mic = MicCollector(ctx)
+    mic.start(mic_callback)
+
+    try:
+        while mic.stream.is_active():
+            await asyncio.sleep(0.01)
+    finally:
+        mic.stop()
+        asr.stop()
+
+# ===============
+# UI
+def is_valid_ip(ip):
+    """
+    检查给定的字符串是否为合法的 IP 地址
+    """
+    pattern = r'^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$'
+    if re.match(pattern, ip):
+        parts = [int(part) for part in ip.split('.')]
+        if all(0 <= part <= 255 for part in parts):
+            return True
+    return False
+
+def get_micro_id2name()->dict[int, str]:
+    p = pyaudio.PyAudio()
+    device_id2name = {}
+    info = p.get_host_api_info_by_index(0)
+    numdevices = info.get('deviceCount')
+    for i in range(0, numdevices):
+        if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+            name = p.get_device_info_by_host_api_device_index(0, i).get('name')
+            device_id2name[i] = name
+    p.terminate()
+    return device_id2name
+
+@ui.page("/")
+def homepage():
+    # Default Setting
+    ctx = SettingAndContext()
+    ctx.api_key = os.environ.get("API_KEY")
+
+    # Declare all ui elements
+    ctl_vrchat_ip = ui.input(
+        label="VRChat IP",
+        placeholder="127.0.0.1",
+        validation={"Invalid IP address": is_valid_ip},
+    ).bind_value(ctx, "vrchat_ip")
+    ctl_vrchat_port = ui.number(
+        label="VRChat OSC port",
+        placeholder="9000",
+        min=0,
+        max=65535,
+        precision=0,
+        step=1,
+    ).bind_value(ctx, "vrchat_port")
+    ctl_osc_bypass_keyboard = ui.checkbox("OSC bypass keyboard").bind_value(ctx, "osc_bypass_keyboard")
+    ctl_osc_enableSFX = ui.checkbox("OSC enable SFX").bind_value(ctx, "osc_enableSFX")
+    ctl_micro_device_id = ui.select(
+        options=get_micro_id2name(),
+        label="Micro Device",
+        with_input=True,
+    ).bind_value(ctx, "micro_device_id")
+    ctl_api_key = ui.input(
+        label="Dashscope API Key",
+    ).bind_value(ctx, "api_key")
+    ctl_disfluency_removal_enabled = ui.checkbox("disfluency_removal_enabled").bind_value(ctx, "disfluency_removal_enabled")
+
+    # TODO test
+
+    start_btn = ui.button("Start").bind_enabled_from(ctx, "stt_worker", lambda worker: worker==None)
+    stop_btn = ui.button("Stop").bind_enabled_from(ctx, "stt_worker", lambda worker: worker!=None)
+
+    def on_start_btn_clicked():
+        ctx.stt_worker = asyncio.create_task(ARSWorker(ctx))
+        ui.update(start_btn)
+        ui.update(stop_btn)
+    start_btn.on_click(on_start_btn_clicked)
+    def on_end_btn_clicked():
+        if ctx.stt_worker != None:
+            ctx.stt_worker.cancel()
+            ctx.stt_worker = None
+            ui.update(start_btn)
+            ui.update(stop_btn)
+    stop_btn.on_click(on_end_btn_clicked)
+
+
+if __name__ in {"__main__", "__mp_main__"}:
+    # ============
+    # Logger
+
+    # Initialize the logger
+    # Note it is using a QueueHandler, meaning the actual log job is finished on another process
+    logger.setLevel(logging.DEBUG)
+    log_queue = multiprocessing.Queue(-1)
+    queue_handler = logging.handlers.QueueHandler(log_queue)
+    logger.addHandler(queue_handler)
+
+    # Disable the progration
+    logger.propagate = False
+
+    # Initialize the QueueListener
+    # Create a file handler and set its level
+    file_log_format = 'VRCPASR: [%(asctime)s - %(filename)s Line %(lineno)d - %(processName)s - %(threadName)s ] [%(levelname)s] %(message)s'
+    file_handler = logging.handlers.RotatingFileHandler('vrchat_paraformer_asr.log', maxBytes=5*1024, backupCount=1)
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(logging.Formatter(file_log_format))
+
+    # Create a console handler and set its level
+    console_log_format = '[%(levelname)s] %(message)s'
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    console_handler.setFormatter(logging.Formatter(console_log_format))
+
+    # Add the handlers to the listener
+    queue_listener = logging.handlers.QueueListener(log_queue, file_handler, console_handler, respect_handler_level=True)
+    queue_listener.start()
+
+
+    # =======================
+    # UI
+    ui.run(reload=True)
